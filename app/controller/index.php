00ff934948b93bc0f063981111157fc4b07caca2
Artem <ttenor@yandex.ru>
2012-08-14 21:23:16 +0400

    Добавлено дефолтное подключение файлов

--- app/controller/index.php
+++ app/controller/index.php
@@ -12,8 +12,14 @@ class index {
 
 		$html = new Blitz(ROOT . '/view/blocks/html/view/html.tpl');
 
+		$files = 
+			file_get_contents('view/includes/libs.tpl') .
+			((DEV) ? file_get_contents('view/includes/developer.tpl') : '') .
+			file_get_contents('view/includes/require.tpl');
+
 		echo $html->parse(array(
 			'title' => 'Заголовок',
+			'files' => $files,
 			'body'  => 'Контент'
 		));
 	}
