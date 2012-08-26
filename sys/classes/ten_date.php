0e9ca2e62c438e5bcbb5e9bca53da3b6d8161662
Artem <ttenor@yandex.ru>
2012-08-26 18:58:00 +0400

    Доработан класс работы с датой и временем

--- sys/classes/ten_date.php
+++ sys/classes/ten_date.php
@@ -5,18 +5,86 @@
 /*	Использование
 
 	Преобразование даты в привычный вид:
-		Без времени:
-			$date = ten_date::get_date('2011-04-28 18:21:02');
-		Со временем:
-			$date_time = ten_date::get_date('2011-04-28 18:21:02', true);
+		$date = ten_date::get_date('2011-04-28 18:21:02', array(
+			
+			'lang'  => 'ru   | en',			// Язык
+			'month' => 'full | short',		// Полная или краткая запись месяца
+			'time'  =>  true | false		// Выводить или нет время
+		));
 */
 
 class ten_date {
 	
-	protected static $month = array(									// Месяца в родительном падеже
-		'01'=>'января','02'=>'февраля','03'=>'марта','04'=>'апреля',
-		'05'=>'мая','06'=>'июня','07'=>'июля','08'=>'августа',
-		'09'=>'сентября','10'=>'октября','11'=>'ноября','12'=>'декабря'
+	private static $month = array(										// Месяца в родительном падеже
+		
+		'ru' => array(
+			
+			'full' => array(
+				'01' => 'января',   '02' => 'февраля', '03' => 'марта',  '04' => 'апреля',
+				'05' => 'мая',      '06' => 'июня',    '07' => 'июля',   '08' => 'августа',
+				'09' => 'сентября', '10' => 'октября', '11' => 'ноября', '12' => 'декабря'
+			),
+
+			'short' => array(
+				'01' => 'янв', '02' => 'фев',  '03' => 'мар',  '04' => 'апр',
+				'05' => 'мая', '06' => 'июня', '07' => 'июля', '08' => 'авг',
+				'09' => 'сен', '10' => 'окт',  '11' => 'ноя',  '12' => 'дек'
+			)
+		),
+
+		'en' => array(
+
+			'full' => array(
+				'01' => 'january',   '02' => 'february', '03' => 'march',    '04' => 'april',
+				'05' => 'may',       '06' => 'june',     '07' => 'july',     '08' => 'august',
+				'09' => 'september', '10' => 'october',  '11' => 'november', '12' => 'december'
+			),
+
+			'short' => array(
+				'01' => 'jan', '02' => 'feb', '03' => 'mar', '04' => 'apr',
+				'05' => 'may', '06' => 'jun', '07' => 'jul', '08' => 'aug',
+				'09' => 'sep', '10' => 'oct', '11' => 'nov', '12' => 'dec'
+			)
+		)
+	);
+
+	private static $words = array(										// Слова
+		
+		'now' => array(
+			'en' => 'now',
+			'ru' => 'сейчас'
+		),
+
+		'today' => array(
+			'en' => 'today',
+			'ru' => 'сегодня'
+		),
+
+		'yesterday' => array(
+			'en' => 'yesterday',
+			'ru' => 'вчера'
+		),
+
+		'tomorrow' => array(
+			'en' => 'tomorrow',
+			'ru' => 'завтра'
+		),
+
+		'will' => array(
+			'en' => 'will',
+			'ru' => 'будет'
+		),
+
+		'at' => array(
+			'en' => 'at',
+			'ru' => 'в'
+		)
+	);
+
+	private static $default_options = array(							// Дефолтные параметры
+		'lang'  => 'ru',
+		'month' => 'full',
+		'time'  => false
 	);
 	
 	public static $now = 300;											// Время в секундах для написания "сейчас"
@@ -24,30 +92,54 @@ class ten_date {
 	/**
 	 * Функция преобразования даты в привычный вид
 	 *
-	 * @param  datetime $date Дата для преобразования
-	 * @param  boolean $time Необходимость добавления времени к дате
+	 * @param  datetime $date    Дата для преобразования
+	 * @param  array    $options Параметры вывода даты
 	 * @return string
 	 */
-	public static function get_date($date, $time = false)
-	{
-		list($year, $month, $daytime) = explode('-', $date);
+	public static function get_date($date, $options = null) {
 		
-		if(substr($daytime, 0, 2) == date('d') && $year . '-' . $month == date('Y-m'))										// Если дата сегодняшняя
-			$ret_date = 'сегодня';
+		foreach(ten_date::$default_options as $key => $val)																	// Установка значений по умолчанию
+			if(!isset($options[$key]))																						// для незаданных опций
+				$options[$key] = $val;
+
+		$lang  = $options['lang'];
+		$words = ten_date::$words;
+		$ret_date = null;																									// Переменная для записи возвращаемой даты
+
+		list($year, $month, $daytime) = explode('-', $date);																// Год, месяц и день со временем
+		$day = (substr($daytime, 0, 1) == '0') ? substr($daytime, 1, 1) : substr($daytime, 0, 2);							// День без первого нуля
 		
-		else if(((substr($daytime, 0, 1) == '0') ? substr($daytime, 1, 1) : substr($daytime, 0, 2)) == (date('j') - 1) && 	// Если дата вчерашняя
-				$year . '-' . $month == date('Y-m'))
-				$ret_date = 'вчера';
+		if($year . '-' . $month == date('Y-m')) {																			// Если текущий год и месяц
+
+			$now_day = date('d');
+
+			if($day == $now_day)																							// Если дата сегодняшняя
+				$ret_date = $words['today'][$lang];
+
+			else if($day == $now_day - 1)																					// Иначе, если дата вчерашняя
+				$ret_date = $words['yesterday'][$lang];
+
+			else if($day == $now_day + 1)																					// Иначе, если дата завтрашняя
+				$ret_date = $words['tomorrow'][$lang];
+		}
 		
-		else $ret_date = ((substr($daytime, 0, 1) == '0') ? substr($daytime, 1, 1) : substr($daytime, 0, 2)) . ' ' . 		// Если дата позавчерашняя и до этого
-						 ten_date::$month[$month] . ' ' . 
-						 (($year == date('Y')) ? '' : $year);																// Если год текущий, то его не надо печатать
+		if(is_null($ret_date))																								// Если дата до сих пор не указана
+			$ret_date = 
+				$day . ' ' .
+				ten_date::$month[$lang][$options['month']][$month] . ' ' . 
+				(($year == date('Y')) ? '' : $year);																		// Если год текущий, то его не надо печатать
 		
-		if($ret_date == 'сегодня' && strtotime('now') - strtotime($date) < ten_date::$now 									// Если дата сегодняшняя и время не далее, чем $now секунд назад
-								  && strtotime('now') - strtotime($date) > 0)												// и разница между текущим временем и переданным положительна
-			return 'сейчас';
+		if(
+			$ret_date == $words['today'][$lang] && 																			// Если дата сегодняшняя
+			strtotime('now') - strtotime($date) < ten_date::$now && 														// и время не далее, чем $now секунд назад
+			strtotime('now') - strtotime($date) > 0																			// и разница между текущим временем и переданным положительна
+		)
+			return $words['now'][$lang];																					// Значит это сейчас
+		
 		else																												// Иначе, либо дата не сегодняшняя, либо разница во времени больше, чем $now секунд
-			return ((strtotime('now') < strtotime($date)) ? 'будет ' : '') . 												// Если надо печатать "будет"
-				$ret_date . (($time == true) ? ' в ' . substr($daytime, 3, 5) : '');										// Если надо печатать время
+			return
+				((strtotime('now') < strtotime($date)) ? $words['will'][$lang] . ' ' : '') . 								// Если надо печатать "будет"
+				$ret_date . 																								// Дата
+				(($options['time'] == true) ? ' ' . $words['at'][$lang] . ' ' . substr($daytime, 3, 5) : '');				// Если надо печатать время
 	}
 }
