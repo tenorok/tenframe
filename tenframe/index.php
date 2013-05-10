373a150d900aa3e1a12cf2862cb6a3ff0408d0b0
tenorok <ttenor@yandex.ru>
2013-05-10 15:43:40 +0400

    Удалена лишняя проверка на открытие не index.php

--- tenframe/index.php
+++ tenframe/index.php
@@ -4,17 +4,9 @@
  * Tenframe - PHP framework: github.com/tenorok/tenframe
  * @copyright 2012–2013 Artem Kurbatov, tenorok.ru
  * @license MIT license
+ * @version 0.0.1
  */
 
 session_start();
-
-require 'core.php';                                     // Подключение ядра
-ten\core::init();
-
-if(
-    $_SERVER['PHP_SELF'] != '/' . TEN_PATH . '/index.php' &&    // Если текущий адрес не index.php
-    preg_match('/\.php$/', $_SERVER['PHP_SELF'])          &&    // а какой-то другой php-файл
-    file_exists(ROOT .     $_SERVER['PHP_SELF'])                // и он существует
-) {
-    ten\route::$called = true;                                  // Маршрут считается проведённым
-}
+require 'core.php';             // Подключение ядра
+ten\core::init();               // Инициализация ядра
