af477a5bd896df7b7f538dfa01cfde8e01ab17b5
tenorok <ttenor@yandex.ru>
2013-06-28 18:56:32 +0400

    JS-ядро и роутинг не нужны

--- mod/shop/init.php
+++ mod/shop/init.php
@@ -1,33 +1,9 @@
 <?php
 
-// Формирование элемента для добавления в роутинг
-
 $settings = \ten\core::requireFile('/mod/admin/conf/settings.php');
 
 $adminpage = ten\text::del($settings['urls']['page'], '/') . '/modshop/categories/';
-$page = $settings['urls']['page'];
 $ctr = 'ten\mod\shop\ctr\\';
-
-ten\file::autogen('/mod/shop/view/include/routes.js', "core.addRoute({
-    url:  [
-        '" . $page . "',
-        '" . $page . "{page}/',
-        '" . $page . "{page}/{tab}'
-    ],
-    ctrl: 'core.mod.shop.categories.controller',
-    func: 'list'
-}, {
-    url: [
-        '/" . $adminpage . "add/',
-        '/" . $adminpage . "{parentid}/addcategory/'
-    ],
-    ctrl: 'core.mod.shop.categories.controller',
-    func: 'add'
-}, {
-    url:  '/" . $adminpage . "{categoryid}/',
-    ctrl: 'core.mod.shop.categories.controller',
-    func: 'edit'
-});");
 
 array_push(ten\route::$routes, array(
     'url'      => array(
