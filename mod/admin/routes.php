6ef14c2aeb58d40fe2a160853a4dda972ba7edff
tenorok <ttenor@yandex.ru>
2013-07-24 01:15:54 +0400

    Модуль admin адаптировани под новую маршрутизацию

---
+++ mod/admin/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ten;
+
+$settings = core::requireFile(__DIR__ . '/conf/settings.php');
+
+$page = text::rdel($settings['urls']['page'], '/');
+$ctr = 'ten\mod\admin\ctr\\';
+
+route::get([
+    'url' => [
+        $page,
+        $page . '/{page}/',
+        $page . '/{page}/{tab}/'
+    ],
+    'call' => $ctr . 'page::page'
+]);
+
+route::post([
+    'url' => $page . '/auth/',
+    'call' => $ctr . 'auth::auth'
+]);
+
+route::post([
+    'url' => $page . '/quit/',
+    'call' => $ctr . 'auth::quit'
+]);
